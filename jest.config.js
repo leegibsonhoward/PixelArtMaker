c74b7f5f312b65a5e7898ed7863f094c417bd31d
Lee Howard <leegibsonhoward@gmail.com>
2020-12-30 01:56:02 -0800

    Add jest tests
    - Mock styles to avoid error on importing scss files

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "roots": [
+      "<rootDir>/src"
+    ],
+    "moduleNameMapper": {
+      "\\.(css|scss)$": "<rootDir>/src/__tests__/__mocks__/styleMock.ts"
+    },
+    "testMatch": [
+      "<rootDir/src/__tests__/**/*.+(ts|tsx|js)",
+      "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    "transform": {
+      "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import './main.scss';
 
 console.log("I'm Alive!");
+
+export const sum
+    = (...a: number[]) =>
+      a.reduce((acc, val) => acc + val, 0);
